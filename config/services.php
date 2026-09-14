97d08e4ed52157468a4b31b1fdf637decdfb673f
HOSSEINAJERLOO <ahosseinajerloo@gmail.com>
2026-09-15 01:17:27 +0430

    sync

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'google' => [
     'client_id' => env('GITHUB_CLIENT_ID'),
     'client_secret' => env('GITHUB_CLIENT_SECRET'),
-    'redirect' => 'http://localhost:8000/auth/google/callback',
+    'redirect' => env('GOOGLECALLBACK'),
 ],
 
 ];
